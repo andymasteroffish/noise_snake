4528b05ccefd93b11d673c8f66293c732d049dfc
Andy Wallace <andymasteroffish@gmail.com>
2016-06-10 18:56:10 -0400

    getting ready for windows!

---
+++ src/Snake.hpp
@@ -0,0 +1,44 @@
+//
+//  Snake.hpp
+//  emptyExample
+//
+//  Created by Andrew Wallace on 6/9/16.
+//
+//
+
+#ifndef Snake_hpp
+#define Snake_hpp
+
+#include "ofMain.h"
+
+class Snake{
+public:
+    
+    void setup(int _gridSize);
+    void update();
+    
+    void move();
+    void eat();
+    
+    void keyPressed(int key);
+    
+    int curX, curY;
+    int xDir, yDir;
+    
+    int framesToMove;
+    int moveTimer;
+    
+    int gridSize;
+    
+    vector<int> trailX;
+    vector<int> trailY;
+    
+    bool canChangeDir;
+    
+    bool justDied;
+    
+    int score;
+    
+};
+
+#endif /* Snake_hpp */
